744ef8c3e8dc46dcdfdebf2519eaa5d2314eb05d
Diego Sousa <derickbenji2@gmail.com>
2021-01-03 21:32:53 -0300

    Add tags HTML

--- pages/post/first-post.tsx
+++ pages/post/first-post.tsx
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import Image from 'next/image'
 
 const Hello = () =>{
-  return (
-      <article>
-        <h1>Hello from the fist post</h1>
-        <Image
-          src="/post/first-post/luffy.jpg"
-          height={427}
-          width={640}
-        />
-        <p>Monkey D. Luffy</p>
-      </article>
+  return(
+    <>
+      <Head>
+        <title>Hello there :: My Blog</title>
+        <meta property="og::title" content="first-post" />
+      </Head>
+        <article>
+          <h1>Hello from the fist post</h1>
+          <Image
+            src="/post/first-post/luffy.jpg"
+            height={427}
+            width={640}
+          />
+          <p>Monkey D. Luffy</p>
+        </article>
+    </>
   )
 }
 
